1a815882779167c0379f20e6b0670146d6b429c4
marcusbegic <marcus.begic@gmail.com>
2020-11-02 12:33:21 +0100

    crude actor implementation

--- src/cactus/core.clj
+++ src/cactus/core.clj
@@ -1,18 +1,28 @@
 
 (ns cactus.core
-  (:require [clojure.core.async :as async])
+  (:gen-class)
+  (:require [clojure.core.async
+             :as async
+             :refer [>! <! >!! <!! go chan buffer close! thread
+                     alts! alts!! timeout put!]]))
+(defn action [arg state]
+        (+ arg state))
 
-  (:gen-class))
+(def chan-1 (async/chan))
+(def chan-2 (async/chan))
+
+(defn actor [chan-1 chan-2]
+  (go (>!! chan-2 (<!! chan-1)))
+ )
+
+(defn print-actor [chan-2]
+    (go (println (<! chan-2)))
+ )
 
 
-(defn action [arg]
-        (+ 3 arg))
-
-(defn actor []
-
-  )
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println (action 3)))
+(defn -main  [& args]
+  (while true
+  (actor chan-1 chan-2)
+  (print-actor chan-2)
+  (>!! chan-1 (Integer/parseInt (read-line) )
+  )))
