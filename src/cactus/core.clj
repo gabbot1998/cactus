62ce954adba345b05efb1a79be5afb090bde1c60
marcusbegic <marcus.begic@gmail.com>
2020-11-04 09:49:08 +0100

    started work on controller

--- src/cactus/core.clj
+++ src/cactus/core.clj
@@ -10,9 +10,6 @@
              :as async
              :refer [>! <! >!! <!! go chan buffer close! thread] ]))
 
-(def chan-1 (async/chan))
-(def chan-2 (async/chan))
-
 (defn score [a b]
   (if (= a "") 0
     (if (= b "") 0
@@ -39,7 +36,20 @@
       (set! nw (<!! w))
   ))))
 
+(defn controller [A B c1 c2 c2 c2 ]
 
+  )
+
+
+(def chan-controller-1 (async/chan))
+(def chan-controller-2 (async/chan))
+(def chan-controller-3 (async/chan))
+(def chan-controller-4 (async/chan))
+(def chan-1-2 (async/chan))
+(def chan-2-3 (async/chan))
+(def chan-3-4 (async/chan))
+(def chan-4-aligner (async/chan))
 
 (defn -main  [& args]
+  
   )
