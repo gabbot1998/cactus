9e0f54e03c85e741eac8f91e45735489d4122b55
Gabriel Borglund <gabrielborglund@Gabriels-MacBook-Air.local>
2020-11-04 11:40:32 +0100

    created the controller

--- src/cactus/core.clj
+++ src/cactus/core.clj
@@ -30,37 +30,56 @@
 
 (defn sw-cell [a b v w]
   (let [nw (atom 0)  n (atom 0)]
-  (go
     (while true
-      (set! n  (cell-action nw n (<!! a) (<!! b)))
-      (set! nw (<!! w))
+      (swap! n  (fn [n] (cell-action nw n (<!! a) (<!! b))))
+      (swap! nw (fn [nw] (<!! w)))
       (>!! v n)
-  ))))
+  )))
 
 
+(defn print-actor [chan]
+    (go (while true
+      (println (<!! chan))
+    ))
+  )
 
-(for [i (range 5)] (println (if (= i 0) ("") (nth B i))))
 
-(defn controller [A B c1 c2 c3 c4 c5 ] ;;c5 is chanel to send b
-      (for [i (range 5)]
-        (if (= i 0) (>!! "") (>!! (nth B i)))
+(defn controller [A B c1 c2 c3 c4 c5] ;;c5 is chanel to send b
+      (doseq [i (range 4)]
+      (do
+        (if (= i 0) (>!! c5 "") (>!! c5 (nth B (- i 1))))
         (>!! c1 "")
         (>!! c2 (nth A 0))
         (>!! c3 (nth A 1))
         (>!! c4 (nth A 2))
+      )
     )
  )
 
 
-(def chan-controller-1 (async/chan))
-(def chan-controller-2 (async/chan))
-(def chan-controller-3 (async/chan))
-(def chan-controller-4 (async/chan))
+(def chan-con-1 (chan 10))
+(def chan-con-2 (async/chan))
+(def chan-con-3 (async/chan))
+(def chan-con-4 (async/chan))
+(def chan-con-5 (async/chan))
 (def chan-1-2 (async/chan))
 (def chan-2-3 (async/chan))
 (def chan-3-4 (async/chan))
 (def chan-4-aligner (async/chan))
 
 (defn -main  [& args]
+  (comment
+  (print-actor chan-con-1)
+  (print-actor chan-con-2)
+  (print-actor chan-con-3)
+  (print-actor chan-con-4)
+  (print-actor chan-con-5)
+  )
+
+  ;;(>!! chan-con-1 "wow")
+  ;;(doseq [i (range 5)] (do (>!! chan-con-1 "m") (>!! chan-con-2 "s") (>!! chan-con-3 "d") (>!! chan-con-4 "a") (>!! chan-con-5 "v")))
+
+
+  ;;(controller "abc" "def" chan-con-1 chan-con-2 chan-con-3 chan-con-4 chan-con-5)
 
   )
