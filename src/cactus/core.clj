52a5f426104c81e65fed1034c109cb5a4a9334a9
Gabriel Borglund <borglund.g@gmail.com>
2020-11-05 16:26:01 +0100

    Implementation of stuff

--- src/cactus/core.clj
+++ src/cactus/core.clj
@@ -9,10 +9,11 @@
   (:require [clojure.core.async
              :as async
              :refer [>! go-loop <! >!! <!! go chan buffer close! thread] ]))
-
+(def match 8)
+(def mismatch -3)
 (def penalty -2)
-(def mismatch -1)
-(def match 5)
+
+
 
 (defn score [a b]
   (if (= a "") 0
@@ -32,7 +33,7 @@
    0)
  )
 
- (defn sw-cell [a b w v name]
+ (defn sw-cell [a b w v aln-v name]
     (go
       (loop [nw 0 n 0 i 0];;Set initial state
         (let [new-a (<!! a) new-b (<!! b) new-w (<!! w)] ;;Wait for ports
@@ -41,7 +42,8 @@
                 new-n (cell-action nw n new-w new-a new-b)
                 ] ;;Assign new local state and execute body
             (>!! v new-n);;Set output
-            (println (str i name ": " new-n "\n\n"))
+            (>!! aln-v new-n)
+            ;(println (str i name ": " new-n "\n\n"))
             (recur new-nw new-n (inc i)) ;;Recur
           )
         )
@@ -55,7 +57,7 @@
         (let [new-str (<!! chan) ];;Wait for ports
           (let [] ;;Assign new local state and execute body
             ;(print "Value of last actor is: ")
-            ;(println new-str)
+            (println new-str)
             ;;Set output
             (recur );;Recur
             )
@@ -64,8 +66,37 @@
       )
   )
 
+(defn trace-back )
 
-(defn controller [A B c1 c2 c3 c4 c51 c52 c53 c54 w] ;;c5 is chanel to send b
+(first (apply min-key second (map-indexed vector [1 2 4 0 5])))
+
+(defn aligner [c1 c2 c3 c4 out]
+  (go
+    (loop [ row 0
+            matrix [[0 0 0 0]
+                    [0 0 0 0]
+                    [0 0 0 0]
+                    [0 0 0 0]]
+            i 0 ;row number biggest elem
+            j 0 ;col number biggest elem
+            max 0 ;current biggest number
+          ]
+
+          (let [ci1 (<!! c1) ci2 (<!! c2) ci3 (<!! c3) ci4 (<!! c4)]
+          (let [new-row (inc row) new-matrix (assoc matrix row [ci1 ci2 ci3 ci4]) ]
+            (if (= row 3) (>!! out new-matrix) )
+            (largest )
+            ;(if (= row 3) (println new-matrix) )
+            (recur new-row new-matrix)
+          )
+
+        )
+      )
+    )
+  )
+
+
+(defn controller [A B c1 c2 c3 c4 c51 c52 c53 c54 w] ;;c51 - c54 is chanel to send b
       (go
 
         (loop [];;Set initial state
@@ -129,30 +160,44 @@
  )
 
 
+(def chan-con-1-zero (chan 10))
 
 (def chan-con-1 (chan 10))
-(def chan-con-b1 (chan 10))
-(def chan-con-1-zero (chan 10))
 (def chan-con-2 (async/chan 10))
-(def chan-con-b2 (async/chan 10))
 (def chan-con-3 (async/chan 10))
-(def chan-con-b3 (async/chan 10))
 (def chan-con-4 (async/chan 10))
+
+(def chan-con-b1 (chan 10))
+(def chan-con-b2 (async/chan 10))
+(def chan-con-b3 (async/chan 10))
 (def chan-con-b4 (async/chan 10))
+
 (def chan-1-2 (async/chan 10))
 (def chan-2-3 (async/chan 10))
 (def chan-3-4 (async/chan 10))
-(def chan-4-aligner (async/chan 10))
+
 (def chan-4-print (async/chan 10))
+
 (def chan-str-1 (chan 10))
 (def chan-str-2 (chan 10))
 
+(def chan-aln-1 (chan 10))
+(def chan-aln-2 (chan 10))
+(def chan-aln-3 (chan 10))
+(def chan-aln-4 (chan 10))
+
+(def chan-stop (chan 10))
+
+
 (defn -main  [& args]
   (print-actor chan-4-print)
-  (sw-cell chan-con-1 chan-con-b1  chan-con-1-zero chan-1-2 "0")
-  (sw-cell chan-con-2 chan-con-b2  chan-1-2 chan-2-3 "1")
-  (sw-cell chan-con-3 chan-con-b3  chan-2-3  chan-3-4 "2")
-  (sw-cell chan-con-4 chan-con-b4  chan-3-4 chan-4-print "3")
+
+  (sw-cell chan-con-1 chan-con-b1  chan-con-1-zero chan-1-2 chan-aln-1 "0")
+  (sw-cell chan-con-2 chan-con-b2  chan-1-2 chan-2-3 chan-aln-2 "1")
+  (sw-cell chan-con-3 chan-con-b3  chan-2-3  chan-3-4 chan-aln-3 "2")
+  (sw-cell chan-con-4 chan-con-b4  chan-3-4 chan-stop chan-aln-4 "3")
+
+  (aligner chan-aln-1 chan-aln-2 chan-aln-3 chan-aln-4 chan-4-print)
 
   (>!! chan-str-1 "abb")
   (>!! chan-str-2 "aaa")
