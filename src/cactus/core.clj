eab42841e4da546827333951086f80f45617c656
Gabriel Borglund <borglund.g@gmail.com>
2020-11-29 15:13:15 +0100

    Working on the sw in cactus impl, althoug we need state to be able to do it

--- src/cactus/core.clj
+++ src/cactus/core.clj
@@ -26,69 +26,58 @@
     )
   )
 
-(defactor print-two-actro [] [X Y] ==> []
-  (defaction X [x] Y [y] ==>
-      (println x y)
-    )
-  )
-
-(defactor print-actor [name] [in] ==> []
-  (defaction in [a] ==>
-    (println "From " name " got value: " a)
-    )
-  )
-
-(defactor has-initial-tokens [] [] ==> [out]
-  (defaction ==>
-    )
-  )
-
-(defactor arg-actor [a b c] [] ==> [out]
-  (defaction ==>
-    ;(println a b c)
-    )
-  )
-
-(defactor has-two-actions [] [in-0 in-1] ==> [out-0 out-1]
-  (defaction in-0 [a] ==>
-      (println "output on out-0")
-      (>>! out-0 a)
-    )
-  (defaction in-1 [b] ==>
-      (println "output on out-1")
-      (>>! out-1 b)
-    )
-  )
-
 
 (defn -main  [& args]
 
-(entities
-  (actor t (has-two-actions ))
-  (actor f0 (has-initial-tokens))
-  (actor f1 (has-initial-tokens))
+  (println "started")
+  (def A "JLKSDFSDFKJSDFÖSJDLFKNNLCNSDLKNLKNFSLJDFÖEKENNDNFNFNFNFFNFNFNNASDFLDSNVLJNLDFJGLSKDFGHJAJFSDHFJGNHFGHVDTHGHJHGKFGHSDGDFVBFGBFGHFHKHG");Kan vara vilken som helst
+  (def B "SCAIJHUFZFTYKBYDLDJDWCYCGKBYSRVRGCFNUFKLENAWRCRZBUAJEVLXXWJUYPYLUTYTEVQBAEQHATLEXYDQSWJNURUBEISJIVTMBDORIFMUACGIPGMRJUSSGZXGSSRBNARKLFDGOFHQCYUYKIWTVRRWEWUQCYDFGHYARHSARWTKVQGVNJJQFRWDYXVZYANBJBOGTQKYNTLOGDBWPNXASKOWZYIXJNPFNDWFYRRIUDGXKVVQKNFOSCYYRPDQXWUESICYKAVILXVXBRGETRIEXKSQHJJJADWXYAANVXZXFLHPMUCEOEVKERCAZZYAYPCWAYQTJYFXJUCFWCJMQLBBHYVRQBOJDSURKSALPRWVKUFUCCSNKDMAHMDCZNVFHULWYMPSILCAXOSLDMHVYZIVTTSMESERKRMOEEMZSMPXWAFGWGCFJUQEMNSTKHWMXNILOTDNJSDHLYIDWZUTTSAHZLPQDVPUFYJYTNMEBUDPKEEWJELTSBNTGZKMQZJDCMKHOFBTYNLOQPZGWSFWDNYJSIPESFQOPXNFRPQSWLGABADQZSSSKKNRSODMRXHWEQYKSINQSOPQUMUWUEYKYYTKGYBSRMWIOPQOITUQJEAYYJTNADDBWTWOEYTNXCRVWHRNRTFQKVLSNJPLBDAMOLFDVDXFRBXNVTDVSTBXIIHNNKGHSWDVQKDIRMZUNVPBVAOJUWQNSNIUOFHDBHGOVPUWMXLYAFXJHFNNCTOIRNYQTCIECJPIDCSZOKPRLYEFJRCCWXUFOAUGXQEVUXPGBDKOHCKYBEJAJBCBSLVKLVOMKDJIPETYVLXUONMHFDWVFFXLLMXWTRIZMONQCUNKUWSRCKQMJSSZDDRPCOWZHSCTJJGHYLHZOAZYSDOYKSFKYPPTASPCFHVGDHBWMHSSTMCFNHJAEHEFDSDITQHBSXJRXVVDBLDAWTNRCOFGMOUUUOOZVOBWGGMHVLXQEUJJOGDQELUPUAHCOISOPRBDLGOPOGVYREMFHGOSBJSPWMNDPSEJTQRPILPDFADXQAUGASETQRHGKAKNYJVPEBWEEXLXOLWUTZEABNUWRQHLNJFRQEKPHBTTNUGOUAVJJPEQJIQFRWYPXVLRAVMCYFEVYXPTQSPAITDYTGZAIYBQQEEUZPDNAEWLUAMFZTTQVRDHRJZOQLQKGGTPPYYNLKDNJPVQTUNFWVNIYQSOXTRUBVODEWJGMTCNSSLRUIFGBSNVNIFTQBXPNTYMEZTFMCVRTFOCYTNGKSXWBWKHVOZPEMURIYKFVFIWCCTMFVYKVHHSAUPKWRBUEEWORHTEEPPZSQIDGLFEIWIVFWWMDJDCNXICACCLMKXIKEBTKIZLGNXLXZZQNIVQINMSDYVEIHDPGIMFDCNCOZYGYJTEQHBNIMLAYAISAAKMXHUDDFZVVANEYLNCMLRUBKNSESVZDJLCIBGUFBERAVNNTJBOQRMVSDGPEEUTZJYIRRLQBRVLUMOPIHUVHNCBNHXWBPMTTFFNZOQYLRFTYBJGGESSLWAJYPMJZVIDPMJXFWVSLFUPFPTUJAIGDDMVDZAEDZUPVCUJMCLZJMHZYCAUKZVIGVROUSLGBIIEFXOPDOXKEXSHEVNOYTRQGBLHAJUDTXMTSPUXJYNWMHSMGCFWHAIRPGEFHJLBRQUWWXCSJOHUDWWQEHGTSMQEXYLZBMTBGQBRXOSDIRRMCSMHQTRRUAWYPPMCCWJSECKWCQUVXUFYSICYXGXNTIBVYUIMQSDTFNHFCKDSKJHPEKLOYQDLCKDGCKRZTXDASIFJNHIFORRQQJONQCCEUMBCPNSEDVVSITCRTAETDTIHDWRJBOPCPYXMFWLYEYNMVZNLBBUYXKUHYPWUUIZMNXTZYNKAVVYJHWUZGJQHHUVTTWPENYZGPYSZJBMDYUQKVCLWSBBONAWQVKGPFWXOIHUIVLHTBVRFJGVOBSYVBMGSCGAEVTRCTOYOLMNZFMTJISOBBICZZWSRWBBQFZMMPLHHBYLEVJMFYIQLHPVAIFNTCHPTAYHZMAZCUGZRBWWDBJDODVSJXEIS") ;En multipppel av 4. I det här fallet 16.
+  (def width 4)
 
-  (actor p0 (print-actor "Actor 0"))
-  (actor p1 (print-actor "Actor 1"))
+  (entities
+    (actor controller (controller-actor width))
+    (actor stripe (stripe-actor width))
 
-  (network
-    (connection (f0 :out) (t :in-0) {:initial-tokens ["Value for 0"]})
-    (connection (f1 :out) (t :in-1) {:initial-tokens ["Value for 1"]})
+    (actor fanout (fanout-actor ))
 
-    (connection (t :out-0) (p0 :in))
-    (connection (t :out-1) (p1 :in))
+    (actor sw0 (sw-cell ))
+    (actor sw1 (sw-cell ))
+    (actor sw2 (sw-cell ))
+    (actor sw3 (sw-cell ))
+
+    (actor aligner (aligner-actor))
+
+    (network
+      (connection (f0 :out) (t :in-0) {:initial-tokens ["Value for 0"]})
+      (connection (f1 :out) (t :in-1) {:initial-tokens ["Value for 1"]})
+
+      (connection (t :out-0) (p0 :in))
+      (connection (t :out-1) (p1 :in))
+      )
     )
-  )
 
-  ; (entities
-  ;   (actor feeder (has-initial-tokens ))
-  ;   (actor printer (print-actor ))
-  ;   (actor takes-arguments (arg-actor "hej" 2 ["wow" "hello"]))
+  ; (go (>! chan-1 A))
+  ; (go (>! chan-2 B))
   ;
-  ;   (network
-  ;     (connection (feeder :out) (printer :in-0) {:initial-tokens [1 2 3]})
-  ;     )
-  ;   )
+  ; (controller chan-1 chan-2 width chan-contr-fan-a chan-contr-stripe-bs)
+  ;
+  ; (stripe-actor chan-contr-stripe-bs (count A) chan-stripe-sw-1 chan-stripe-sw-2 chan-stripe-sw-3 chan-stripe-sw-4)
+  ;
+  ; (fan-out-actor chan-contr-fan-a chan-fan-sw-1 chan-fan-sw-2 chan-fan-sw-3 chan-fan-sw-4)
+  ;
+  ;
+  ; (sw-cell chan-fan-sw-1 chan-stripe-sw-1 (count A) chan-sw-4-1 chan-sw-1-2  chan-sw-out-1 "1")
+  ; (sw-cell chan-fan-sw-2 chan-stripe-sw-2 (count A) chan-sw-1-2 chan-sw-2-3 chan-sw-out-2 "2")
+  ; (sw-cell chan-fan-sw-3 chan-stripe-sw-3 (count A) chan-sw-2-3 chan-sw-3-4 chan-sw-out-3 "3")
+  ; (sw-cell-end chan-fan-sw-4 chan-stripe-sw-4 (count A) chan-sw-3-4 chan-sw-4-1 chan-sw-out-4 "4" (* (/ (count B) width ) (count A)))
+  ;
+  ; (aligner A B width chan-sw-out-1 chan-sw-out-2 chan-sw-out-3 chan-sw-out-4 chan-res)
+  ;
+  ; (print-actor chan-res)
+  ;
+  ;
+  ;
+  ; (while true )
 
 
   (while true )
