672171aa153d32eceabeb5a5f543c2a33c757f28
Gabriel Borglund <borglund.g@gmail.com>
2020-11-30 16:24:10 +0100

    Added the functionality of state

--- src/cactus/core.clj
+++ src/cactus/core.clj
@@ -14,38 +14,50 @@
 
      [cactus.actor_macros
      :as cactus.actors
-     :refer [defactor entities actor connection network defaction >>! guard]
+     :refer [defactor entities actor connection network defaction >>! guard defstate --]
      ]
 
      )
    )
-
-(defactor guarded-actor [] [] ==> []
-  (defaction ==> (guard true)
-    (println "This actor always fires")
-    )
-  )
-
-(defactor print-two-actor [] [X Y] ==> []
-  (defaction X [x] Y [y] ==>
-      (println x y)
-    )
-  )
-
+;
+; (defactor guarded-actor [] [] ==> []
+;   (defaction ==> (guard true)
+;     (println "This actor always fires")
+;     )
+;   )
+;
+; (defactor print-two-actor [] [X Y] ==> []
+;   (defaction X [x] Y [y] ==>
+;       (println x y)
+;     )
+;   )
+;
 (defactor print-actor [] [in-0] ==> []
   (defaction in-0 [a b c] ==>
-    (println "a, b, c: " a ", " b ", " c)
+    ;(println "a, b, c: " a ", " b ", " c)
     )
   )
-
+;
 (defactor has-initial-tokens [] [] ==> [out]
   (defaction ==>
     )
   )
-
+;
 (defactor arg-actor [a b c] [] ==> [out]
+  ;(defstate [])
   (defaction ==>
     ;(println a b c)
+    )
+  )
+
+(defactor stateful-actor [a] [] ==> [out]
+  (defstate [b 2 a 13 c a])
+  (defaction ==>
+    (println @b)
+    (-- b (inc @b))
+    ;(vreset! b (inc @b))
+    (while (> @b 5))
+    ; (println b)
     )
   )
 
@@ -54,6 +66,7 @@
       (println "output on out-0")
       (>>! out-0 a)
     )
+
   (defaction in-1 [b] ==>
       (println "output on out-1")
       (>>! out-1 b)
@@ -62,11 +75,11 @@
 
 
 (defn -main  [& args]
-
   (entities
+    (actor stateful (stateful-actor "Assigned to a"))
     (actor feeder (has-initial-tokens ))
     (actor printer (print-actor ))
-    ;(actor takes-arguments (arg-actor "hej" 2 ["wow" "hello"]))
+    ; (actor takes-arguments (arg-actor "hej" 2 ["wow" "hello"]))
 
     (network
       (connection (feeder :out) (printer :in-0) {:initial-tokens [1 2 3]})
