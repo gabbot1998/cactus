221c76aecf26cdfe205a010a44d10d0844462c61
Gabriel Borglund <gabrielborglund@MacBook-Air-som-tillhor-Gabriel.local>
2020-12-17 17:34:27 +0100

    Changed a lot of stuff.

--- src/cactus/actor_macros.clj
+++ src/cactus/actor_macros.clj
@@ -1,190 +1,23 @@
 (ns cactus.actor_macros
   (:gen-class)
-(:require
+  (:require
 
 
-   [clojure.core.async
-    :as async
-    :refer [<! >!]
-    ]
+     [clojure.core.async
+      :as async
+      :refer [<! >!]
+      ]
 
-  [cactus.async
-   :as cactus.async
-   :refer [size? go <<! chan]
-   ]
+    [cactus.async
+     :as cactus.async
+     :refer [size? go <<! chan]
+     ]
 
-             )
+     )
    )
 
-(defn is-nil?
-  [map key1 key2]
-
-  (if (= (map key1) nil)
-    true
-    (= ((map key1) key2) nil)
-    )
-  )
-
-(defn assoc-connections
-  [connections-map con-0 con-1 channel]
-
-  (let [connector-0 (keyword (first con-0))
-        connector-1 (keyword (first con-1))
-        port-0 (second con-0)
-        port-1 (second con-1)
-        ]
-
-        (if (is-nil? connections-map connector-0 port-0)
-          ;If there is nothing connected to this port
-          (if (is-nil? connections-map connector-1 port-1)
-            (assoc connections-map
-              connector-0 (conj {port-0 channel} (connections-map connector-0))
-              connector-1 (conj {port-1 channel} (connections-map connector-1))
-              )
-            (throw (Exception. (str "There is already a channel connected to the actor: " connector-1 ", at port: " port-1)))
-            )
-          ;If the connection is already established
-          (throw (Exception. (str "There is already a channel connected to the actor: " connector-0 ", at port: " port-0)))
-          )
-    )
-  )
-
-(defn create-connections-map
-  [network]
-
-  (loop [i 0
-         connection (first network)
-         rest-nw (rest network)
-         connections-map {}
-         arguments-map {} ;Has the structure {:channel-0 {:initial-tokens [1 0 0 2]}}
-
-         current-connections-args (if (and (= (class connection) clojure.lang.PersistentList) (= (class (last connection)) clojure.lang.PersistentArrayMap)) (last connection) nil)
-         connector-0 (if (= (class connection) clojure.lang.PersistentList) (nth connection 1 nil) nil)
-         connector-1 (if (= (class connection) clojure.lang.PersistentList) (nth connection 2 nil) nil)
-        ]
-
-        (if (not= connection 'network)
-          (do
-            (assert (or (= (count connection) 3) (= (count connection) 4)) "Connections take two ports and an optional ArrayMap of arguments.")
-            (assert (or (= (nth connection 0 nil) 'con) (= (nth connection 0 nil) 'for)) "Only connections, networks or for loops should be declared inside the network block.")
-            (assert (nth connection 1 nil) "The connection needs two ports.")
-            (assert (nth connection 2 nil) "The connection needs two ports.")
-            (assert (or (= nil (nth connection 3 nil)) (= (class (nth connection 3 nil)) clojure.lang.PersistentArrayMap)) (str "The last arguemnt has to be an ArrayMap, was: " (class (nth connection 3 nil))))
-            )
-          )
-
-          (if (not= rest-nw ())
-            ;If we have not reached the network token we are not done.
-            (let [new-connection (first rest-nw)
-                  new-rest-nw (rest rest-nw)
-                  new-channel (symbol (str "channel-" i))
-                  new-arguments-map (assoc arguments-map (keyword new-channel) current-connections-args)
-                  new-connections-map (assoc-connections connections-map connector-0 connector-1 new-channel)
-
-                  new-current-connections-args (if (and (= (class new-connection) clojure.lang.PersistentList) (= (class (last new-connection)) clojure.lang.PersistentArrayMap)) (last new-connection) nil)
-                  new-connector-0 (if (= (class new-connection) clojure.lang.PersistentList) (nth new-connection 1 nil) nil)
-                  new-connector-1 (if (= (class new-connection) clojure.lang.PersistentList) (nth new-connection 2 nil) nil)
-                  ]
-
-                  (recur (inc i) new-connection new-rest-nw new-connections-map new-arguments-map new-current-connections-args new-connector-0 new-connector-1)
-              )
-
-              ;If we have reached the network token we return the connections-map
-              (do ;(println   (assoc (assoc connections-map :number-of-channels i) :channel-arguments arguments-map))
-                (assoc (assoc connections-map :number-of-channels i) :channel-arguments arguments-map)
-                )
-            )
-      )
-  )
-
-(defn network-builder
-  [network]
-
-  (if (not= (first network) 'network)
-    (throw (Exception. (str "The last list inside the entities block has to be a network.")))
-    (create-connections-map (reverse network))
-    )
-  )
-
-(defn create-channel-constructor-calls
-  [n channel-arguments]
-
-  (loop [i 0
-         accumulator '()
-         initial-tokens (if (not= nil (channel-arguments (keyword (str "channel-" i)))) ((channel-arguments (keyword (str "channel-" i))) :initial-tokens) [])
-        ]
-
-        (if (= i n)
-          accumulator
-          (recur (inc i) (conj accumulator `( ~(symbol (str "channel-" i)) (chan ~initial-tokens))) (if (not= nil (channel-arguments (keyword (str "channel-" (inc i))))) ((channel-arguments (keyword (str "channel-" (inc i)))) :initial-tokens) []) )
-          )
-        )
-  )
-
-(defmacro call
-  [function & args]
-
-  `(~function ~args)
-  )
-
-(defn create-let-with-channels
-  [n channel-arguments body connections]
-
-  ;; what has to happen inside of the let is a two stage rocket.
-  ; First eval all actors. Then give them the connections map,
-  ; then evaluate again.
-  ;(( (eval (actor p1 (print-one "s"))) {:p1 {:in (chan [1])}}) )
-
-  `(let ~(vec (apply concat (create-channel-constructor-calls n channel-arguments)))
-      ;(println (~(first body) ~connections))
-      ~@(for [act body]
-        (if (= (first act) 'actor)
-          `((~act ~connections))
-          `(println ~act)
-          )
-          )
-      )
-  )
-;
-; (clojure.core/let [channel-0 (cactus.async/chan [420])]
-;   (
-;     (
-;     ((actor p0 (print-one s)) {:feed {:out channel-0}, :p0 {:in channel-0}, :number-of-channels 1, :channel-arguments {:channel-0 {:initial-tokens [420]}}})
-;     )
-;
-;     (clojure.core/println nice)
-;     )
-;     )
-
-; (clojure.core/let [channel-0 (cactus.async/chan [420])]
-; ((
-;   (
-;   (actor p0 (print-one s)) {:feed {:out channel-0}, :p0 {:in channel-0}, :number-of-channels 1, :channel-arguments {:channel-0 {:initial-tokens [420]}}})
-;   )
-;   nil))
-
-
-(defmacro actor
-  [var-name [actor-type & args]]
-
-  (if (= args nil)
-    `#(~actor-type (% ~(keyword var-name)))
-    ; (list ('fn '[connections] `(~actor-type 'connections)))
-    `#(~actor-type ~@args (% ~(keyword var-name)))
-    )
-  )
-
-(defn vect
-  [v?]
-  (if (vector? v?)
-    v?
-    [v?]
-  )
-  )
-
-(defn defnetwork
+(defn exec-network
   [& connections]
-
 
   (let [connections-map (loop
                           [connections (first connections)
@@ -209,7 +42,6 @@
                           )
          keys (keys connections-map)
         ]
-        (println "starting the actors")
 
         (doseq [key keys]
           (let [connections (connections-map key)
@@ -221,54 +53,19 @@
             )
 
           )
-        (println "started all of the actors")
 
     )
   )
-
-(defmacro entities
-  [& actors-then-network]
-
-  (let [connections (network-builder (first (reverse actors-then-network)))
-        actors-list (butlast actors-then-network)
-        calls-to-actors (loop [new-actors-list actors-list
-                               accumulator '()
-                              ]
-
-
-                              ;(println connections)
-                              ;(println new-actors-list)
-
-                              )
-
-        execute (create-let-with-channels (connections :number-of-channels) (connections :channel-arguments) actors-list connections)
-        ]
-
-        (println execute)
-
-        execute
-   )
-   )
-
-
-(defmacro network
-  [& connections]
-
-  (assert nil "network defined outside (entities ...) block.")
-  )
-
-
 
 (defmacro con
   [from to & arguments-map]
 
-  `(let [channel# (chan []) ]
-    {:keys [(keyword (str ~(first from))), (keyword (str ~(first to)))]
-     (keyword (str ~(first from))) {~(keyword (second from)) channel# :ref ~(first from)}
-     (keyword (str ~(first to))) {~(keyword (second to)) channel# :ref ~(first to)}
-    }
-    )
-
+    `(let [channel# (chan []) ]
+      {:keys [(keyword (str ~(first from))), (keyword (str ~(first to)))]
+       (keyword (str ~(first from))) {~(keyword (second from)) channel# :ref ~(first from)}
+       (keyword (str ~(first to))) {~(keyword (second to)) channel# :ref ~(first to)}
+       }
+      )
   )
 
 (defmacro >>!
@@ -413,6 +210,28 @@
         )
   )
 
+
+
+(defmacro defnetwork
+  [& list-to-parse]
+
+  (let [[body-and-guard bindings] (loop [parse list-to-parse
+                                         bindings '()
+                                         ]
+
+                                         (assert parse "End the defaction bindings with a ==>.")
+                                         (if (= (first parse) '==>)
+                                           [(rest parse)
+                                            (reverse bindings)
+                                            ]
+                                            (recur (rest parse) (conj bindings (first parse)))
+                                            )
+                                          )
+        ]
+        (expand-action bindings  (conj `( (~(symbol "exec-network") ~@body-and-guard ) ) 'do))
+        )
+  )
+
 (defmacro defstate
   [bindingsvector]
 
@@ -467,7 +286,7 @@
         )
   )
 
-(defmacro defactor
+(defmacro defentity
  [name parameters connections-in arrow connections-out & state?-and-actions]
 
  (assert state?-and-actions (str "Actor: " name " has to have at least one action." ))
@@ -480,4 +299,17 @@
         )
       )
     )
+ )
+
+
+(defentity output-actor [out-c] [out] ==> []
+ (defaction out [token] ==>
+     (>! out-c token)
+   )
+ )
+
+(defmacro endport
+ [channel]
+
+ `(output-actor (~(symbol "connections-map") ~(keyword (str channel))))
  )
--- src/cactus/core.clj
+++ src/cactus/core.clj
@@ -14,13 +14,13 @@
 
      [cactus.actor_macros
      :as cactus.actors
-     :refer [defactor entities actor con network defaction >>! guard defstate -- defnetwork vect]
+     :refer [defentity con defaction >>! guard defstate -- exec-network defnetwork endport]
      ]
 
      )
    )
 
-(defactor feed-one [send] [] ==> [out]
+(defentity feed-one [send] [] ==> [out]
   (defstate [fired true])
   ;(println "fired")
   (defaction ==> (guard @fired)
@@ -31,13 +31,13 @@
 
   )
 
-(defactor pe [] [in-0 in-1] ==> []
+(defentity pe [] [in-0 in-1] ==> []
   (defaction in-0 [token0] in-1 [token1] ==>
       (println token0 token1)
     )
   )
 
-(defactor incr [i] [in] ==> [out]
+(defentity incr [i] [in] ==> [out]
   ;(println "incr" i "\n\n\n")
   (defaction in [a] ==>
     ;(println "incremented" a)
@@ -45,65 +45,176 @@
     )
   )
 
-(defactor printer [] [in] ==> []
+(defentity printer [] [in] ==> []
   ;(println "printer")
   (defaction in [token] ==>
       (println token)
     )
   )
 
+; (defentity endpoint [s] [] ==> []
+;   (println s)
+;   (defaction ==>
+;     )
+;   )
+;
+; (def endpoint  (endpoint ))
+
+; (defentity nw [] [in] ==> [out]
+;   (defaction in [n] ==>
+;     (defnetwork
+;       (let [incrementers (for [i (range n)] (incr i ))
+;             pr (printer )
+;             feed (feed-one 0 )
+;             out-a (endpoint "nice")
+;             ]
+;
+;             (concat
+;               (list
+;                 (con (feed out) ((nth incrementers 0 nil) in) )
+;
+;                 )
+;
+;               (for [i (range (dec n))]
+;                   (con ((nth incrementers i nil) out) ((nth incrementers (inc i) nil) in) )
+;                 )
+;                 (list   (con ((nth incrementers (dec n) nil) out) (out-a out)))
+;               )
+;             )
+;         )
+;     )
+;   )
+
+(defentity relay [] [in] ==> [out]
+  (defaction in [a] ==>
+      (>>! out a)
+    )
+  )
+
+;#object[cactus.channels.DataFlowChannel 0x3b6a63eb cactus.channels.DataFlowChannel@3b6a63eb]
+
+;Endpoint should exapnd to
+
+(defentity nw1 [] [in] ==> [output]
+  (defnetwork in [x] ==>
+
+      (let [feed (feed-one x)
+            rel (relay )
+            end (endport output)
+            pr (printer )
+           ]
+
+        (list
+          (con (feed out) (rel in))
+          (con (rel out) (end out))
+          )
+
+      )
+    )
+  )
+
+(defentity nw2 [] [in] ==> [output]
+  (defaction in [x] ==>
+    (exec-network
+      (let [feed (feed-one x)
+            rel (relay )
+            end (endport output)
+            ;ending (printboi (connections-map :output))
+            pr (printer )
+           ]
+           ;(println x)
+        ;(println  "The output is: " (connections-map :output))
+
+        (list
+          (con (feed out) (rel in))
+          (con (rel out) (end out))
+          )
+        )
+      )
+    )
+  )
+
+
+
+
 (def a (feed-one "nice") )
 (def b (pe ) )
 
 ;(to get the reference to an object, we use the {(keyword (str a))  a})
-
+; {
+;   :cactus.core$feed_one$fn__9609@1a5ea4cf {:ref #object[cactus.core$feed_one$fn__9609 0x1a5ea4cf cactus.core$feed_one$fn__9609@1a5ea4cf], :out #object[cactus.channels.DataFlowChannel 0x41030c21 cactus.channels.DataFlowChannel@41030c21]}
+;   :cactus.core$incr$fn__9774@33e80a27 {:ref #object[cactus.core$incr$fn__9774 0x33e80a27 cactus.core$incr$fn__9774@33e80a27], :in #object[cactus.channels.DataFlowChannel 0x41030c21 cactus.channels.DataFlowChannel@41030c21], :out #object[cactus.channels.DataFlowChannel 0x3c5797c6 cactus.channels.DataFlowChannel@3c5797c6]}
+;   :cactus.core$printer$fn__9848@7649d350 {:ref #object[cactus.core$printer$fn__9848 0x7649d350 cactus.core$printer$fn__9848@7649d350], :in #object[cactus.channels.DataFlowChannel 0x3c5797c6 cactus.channels.DataFlowChannel@3c5797c6]}
+; }
 
 (defn -main  [& args]
 
   ; (defnetwork
-  ;
+  ;   (let [feed0 (feed-one 10 ) netw (nw ) printer (printer )]
   ;   (list
-  ;     (con ((feed-one "wap" {}) out) ((printer {}) in) )
-  ;     (con ((feed-one "wap2" {}) out) ((printer {}) in) )
+  ;     (con (feed0 out) (netw    in))
+  ;     (con (netw out) (endpoint out) )
+  ;     (con (endpoint out-c) (printer in))
+  ;     ;(con (feed1 out) (printer in) )
+  ;     )
   ;     )
   ;   )
 
   ;
+
+  (exec-network
+    (let [feed (feed-one "19759823042038457923845")
+          nw1 (nw1 )
+          pr (printer )
+          ]
+
+          (list
+            (con (feed out) (nw1 in))
+            (con (nw1 output) (pr in))
+            )
+      )
+    )
+
   ; (defnetwork
-  ;   (let [incre (incr {})
-  ;         printer (printer {})
-  ;         feed (feed-one 0 {})
+  ;   (let [incre (incr 1)
+  ;         printer (printer )
+  ;         feed (feed-one 0 )
   ;         ]
   ;
   ;     (list
   ;       (con (feed out) (incre in) )
-  ;       (con (incre out) (printer in))
+  ;       (con (incre out) (endpoint out))
   ;       )
   ;     )
   ;   )
 
-  (def n 620)
+; {
+;   :cactus.core$nw1$fn__10144@5fbb2e1c {:ref #object[cactus.core$nw1$fn__10144 0x5fbb2e1c cactus.core$nw1$fn__10144@5fbb2e1c], :out #object[cactus.channels.DataFlowChannel 0xb257072 cactus.channels.DataFlowChannel@b257072]},
+;   :cactus.core$nw2$fn__10219@138d85f8 {:ref #object[cactus.core$nw2$fn__10219 0x138d85f8 cactus.core$nw2$fn__10219@138d85f8], :in #object[cactus.channels.DataFlowChannel 0xb257072 cactus.channels.DataFlowChannel@b257072]}
+; }
   ;
-  (defnetwork
-    (let [incrementers (for [i (range n)] (incr i ))
-          pr (printer )
-          feed (feed-one 0 )
-          ]
+;
+; {
+;   :cactus.core$feed_one$fn__9609@295e989 {:ref #object[cactus.core$feed_one$fn__9609 0x295e989 cactus.core$feed_one$fn__9609@295e989], :out #object[cactus.channels.DataFlowChannel 0x23b62cc3 cactus.channels.DataFlowChannel@23b62cc3]}
+;   :cactus.core$nw1$fn__10212@504216ff {:ref #object[cactus.core$nw1$fn__10212 0x504216ff cactus.core$nw1$fn__10212@504216ff], :in #object[cactus.channels.DataFlowChannel 0x23b62cc3 cactus.channels.DataFlowChannel@23b62cc3], :output #object[cactus.channels.DataFlowChannel 0xf4e235e cactus.channels.DataFlowChannel@f4e235e]}
+;   :cactus.core$printer$fn__9848@4ce4c097 {:ref #object[cactus.core$printer$fn__9848 0x4ce4c097 cactus.core$printer$fn__9848@4ce4c097], :in #object[cactus.channels.DataFlowChannel 0xf4e235e cactus.channels.DataFlowChannel@f4e235e]}
+; }
+;
+;
+;
+; {
+;   :cactus.core$feed_one@3bda821c {:ref #object[cactus.core$feed_one 0x3bda821c cactus.core$feed_one@3bda821c], :out #object[cactus.channels.DataFlowChannel 0x10b8be07 cactus.channels.DataFlowChannel@10b8be07]}
+;   :cactus.core$relay$fn__10144@7a228301 {:ref #object[cactus.core$relay$fn__10144 0x7a228301 cactus.core$relay$fn__10144@7a228301], :in #object[cactus.channels.DataFlowChannel 0x10b8be07 cactus.channels.DataFlowChannel@10b8be07], :out #object[cactus.channels.DataFlowChannel 0x7e6c1d9a cactus.channels.DataFlowChannel@7e6c1d9a]}
+;   :cactus.core$endpoint@78e545fe {:ref #object[cactus.core$endpoint 0x78e545fe cactus.core$endpoint@78e545fe], :output #object[cactus.channels.DataFlowChannel 0x7e6c1d9a cactus.channels.DataFlowChannel@7e6c1d9a]}
+; }                                                                                                              :output #object[cactus.channels.DataFlowChannel 0xf4e235e cactus.channels.DataFlowChannel@f4e235e]
 
-          (concat
-            (list
-              (con (feed out) ((nth incrementers 0 nil) in) )
+;Endpoint should take the token at port out and add it to the channel at (>! (connections-map :out))
 
-              )
-
-            (for [i (range (dec n))]
-                (con ((nth incrementers i nil) out) ((nth incrementers (inc i) nil) in) )
-              )
-              (list   (con ((nth incrementers (dec n) nil) out) (pr in)))
-            )
-          )
-      )
-
+; (defentity endpoint [] [output] ==> []
+;   (defaction output [token] ==>
+;     ;(>>! token output)
+;     )
+;   )
   ; (defnetwork
   ;   (let [feeders (for [i (range n)] (feed-one i {}))
   ;         printers (for [i (range n)] (printer {}))
